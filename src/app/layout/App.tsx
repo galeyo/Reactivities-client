8be6a4bd114bb5e7ea4ca9fc8aa6c5087a7795d9
e_gokhap <galey_okhapkin@kb.cz>
2019-11-04 00:12:53 +0100

    finish section 13

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './../../features/nav/NavBar';
 import ActivityDashboard from './../../features/activities/dashboard/ActivityDashboard';
@@ -10,10 +10,29 @@ import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import { RouteComponentProps, Switch } from 'react-router';
 import NotFound from './NotFound';
 import {ToastContainer} from 'react-toastify';
+import LoginForm from './../../features/user/LoginForm';
+import { RootStoreContext } from '../stores/rootStore';
+import { useEffect } from 'react';
+import LoadingComponent from './LoadingComponent';
+import ModalContainer from './../common/modals/ModalContainer';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
+	const rootStore = useContext(RootStoreContext);
+	const {setAppLoaded, token, appLoaded} = rootStore.commonStore;
+	const {getUser} = rootStore.userStore;
+
+	useEffect(() => {
+		if(token) {
+			getUser().finally(() => setAppLoaded())
+		} else {
+			setAppLoaded();
+		}
+	}, [getUser, setAppLoaded, token]);
+
+	if(!appLoaded) return <LoadingComponent content='Loading app ...' />
   return (
 		<Fragment>
+		<ModalContainer />
 		<ToastContainer position='bottom-right' />
       <Route exact path='/' component={HomePage} />
       <Route
@@ -31,6 +50,7 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
                   path={['/createActivity', '/manage/:id']}
                   component={ActivityForm}
                 />
+                <Route path='/login' component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
