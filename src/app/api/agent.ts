39685faf8f48b88edc893b3cea8747883dc2bb20
e_gokhap <galey_okhapkin@kb.cz>
2019-11-17 23:44:10 +0100

    finish section 17

--- src/app/api/agent.ts
+++ src/app/api/agent.ts
@@ -3,7 +3,7 @@ import { IActivity } from './../models/activity';
 import { history } from './../../index';
 import { toast } from 'react-toastify';
 import { IUser, IUserFormValues } from './../models/user';
-import { IProfile } from './../models/profile';
+import { IProfile, IPhoto } from './../models/profile';
 
 axios.defaults.baseURL = 'https://localhost:5001/api';
 axios.interceptors.request.use(
@@ -65,7 +65,16 @@ const requests = {
     axios
       .delete(url)
       .then(sleep(1000))
-      .then(responseBody)
+      .then(responseBody),
+  postForm: (url: string, file: Blob) => {
+    let formData = new FormData();
+    formData.append('File', file);
+    return axios
+      .post(url, formData, {
+        headers: { 'Content-type': 'multipart/form-data' }
+      })
+      .then(responseBody);
+  }
 };
 
 const Activities = {
@@ -89,7 +98,11 @@ const User = {
 
 const Profiles = {
   get: (username: string): Promise<IProfile> =>
-    requests.get(`/profiles/${username}`)
+    requests.get(`/profiles/${username}`),
+  uploadPhoto: (photo: Blob): Promise<IPhoto> =>
+    requests.postForm(`/photos`, photo),
+  setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+  deletePhoto: (id: string) => requests.delete(`/photos/${id}`)
 };
 
 export default { Activities, User, Profiles };
--- src/features/profiles/ProfilePhotos.tsx
+++ src/features/profiles/ProfilePhotos.tsx
@@ -1,23 +1,90 @@
-import React, { useContext } from 'react';
-import { Tab, Header, Card, Image } from 'semantic-ui-react';
+import React, { useContext, useState } from 'react';
+import { Tab, Header, Card, Image, Button, Grid } from 'semantic-ui-react';
 import { RootStoreContext } from '../../app/stores/rootStore';
+import PhotoUploadWidget from './../../app/common/photoUpload/PhotoUploadWidgets';
+import { observer } from 'mobx-react-lite';
 
 const ProfilePhotos = () => {
   const rootStore = useContext(RootStoreContext);
-  const { profile } = rootStore.profileStore;
+  const {
+    profile,
+    isCurrentUser,
+    uploadPhoto,
+    uploadingPhoto,
+    setMainPhoto,
+    deletePhoto,
+    loading
+  } = rootStore.profileStore;
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
+  const [target, setTarget] = useState<string | undefined>(undefined);
+  const [deleteTarget, setDeleteTarget] = useState<string | undefined>(
+    undefined
+  );
+  const handleUploadPhoto = (photo: Blob) => {
+    uploadPhoto(photo).then(() => setAddPhotoMode(false));
+  };
   return (
     <Tab.Pane>
-      <Header icon='image' content='Photos' />
-      <Card.Group itemsPerRow={5}>
-        {profile &&
-          profile.photos.map(photo => (
-            <Card key={photo.id}>
-              <Image src={photo.url} />
-            </Card>
-          ))}
-      </Card.Group>
+      <Grid>
+        <Grid.Column width={16} style={{ paddingBottom: 0 }}>
+          <Header floated='left' icon='image' content='Photos' />
+          {isCurrentUser && (
+            <Button
+              floated='right'
+              basic
+              content={addPhotoMode ? 'Cancel' : 'Add Photo'}
+              onClick={() => setAddPhotoMode(!addPhotoMode)}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {addPhotoMode ? (
+            <PhotoUploadWidget
+              uploadPhoto={handleUploadPhoto}
+              loading={uploadingPhoto}
+            />
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              {profile &&
+                profile.photos.map(photo => (
+                  <Card key={photo.id}>
+                    <Image src={photo.url} />
+                    {isCurrentUser && (
+                      <Button.Group fluid widths={2}>
+                        <Button
+                          name={photo.id}
+                          basic
+                          positive
+                          content='Main'
+                          onClick={e => {
+                            setMainPhoto(photo);
+                            setTarget(e.currentTarget.name);
+                          }}
+                          disabled={photo.isMain}
+                          loading={loading && target === photo.id}
+                        />
+                        <Button
+                          name={photo.id}
+                          disabled={photo.isMain}
+                          onClick={e => {
+                            deletePhoto(photo);
+                            setDeleteTarget(e.currentTarget.name);
+                          }}
+                          loading={loading && deleteTarget === photo.id}
+                          basic
+                          negative
+                          icon='trash'
+                        />
+                      </Button.Group>
+                    )}
+                  </Card>
+                ))}
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
     </Tab.Pane>
   );
 };
 
-export default ProfilePhotos;
+export default observer(ProfilePhotos);
