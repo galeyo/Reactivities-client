4283c6495a1cef829e6d03c6e01107f529f91803
e_gokhap <galey_okhapkin@kb.cz>
2019-11-24 23:17:30 +0100

    finish section 21

--- src/app/stores/profilesStore.ts
+++ src/app/stores/profilesStore.ts
@@ -1,6 +1,6 @@
 import { RootStore } from './rootStore';
 import { observable, action, runInAction, computed, reaction } from 'mobx';
-import { IProfile, IPhoto } from './../models/profile';
+import { IProfile, IPhoto, IUserActivity } from './../models/profile';
 import agent from '../api/agent';
 import { toast } from 'react-toastify';
 export default class ProfileStore {
@@ -25,7 +25,9 @@ export default class ProfileStore {
   @observable uploadingPhoto = false;
   @observable loading = false;
   @observable followings: IProfile[] = [];
-  @observable activeTab: number = 0;
+	@observable activeTab: number = 0;
+	@observable userActivities: IUserActivity[] = [];
+	@observable loadingActivities = false;
 
   @computed get isCurrentUser() {
     if (this.rootStore.userStore.user && this.profile) {
@@ -34,7 +36,21 @@ export default class ProfileStore {
       return false;
     }
   }
-
+	@action loadUserActivities = async (username: string, predicate?: string) => {
+		this.loadingActivities = true;
+		try {
+			const activities = await agent.Profiles.listActivities(username, predicate!);
+			runInAction(() => {
+				this.userActivities = activities;
+			})
+		} catch (error) {
+			toast.error('Problem loading activities');
+		} finally {
+			runInAction(() => {
+				this.loadingActivities = false;
+			})
+		}
+	}
   @action setActiveTab = (activeIndex: number) => {
     this.activeTab = activeIndex;
   };
